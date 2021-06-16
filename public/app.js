a7f5195ebbf9028597256fe72fdb70d5e13435ba
Melinda <melinda.sava.td@gmail.com>
2021-06-16 14:07:53 +0200

    end of netNinja #11video DOM and type casting

--- public/app.js
+++ public/app.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("uuu, i'm out");
+//dom interaction
+//const anchor = document.querySelector('a')!;
+//do a runtime check before accessing dom elements properties, it will only log if the element exists
+//typescript does not know if element exists
+// if(anchor){
+//   console.log(anchor.href);
+// }
+//console.log(anchor.href);
+// const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+//inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // form.children.forEach(child => {  
+    // })
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,38 @@
-console.log("uuu, i'm out");
+//dom interaction
+//const anchor = document.querySelector('a')!;
+
+//do a runtime check before accessing dom elements properties, it will only log if the element exists
+//typescript does not know if element exists
+// if(anchor){
+//   console.log(anchor.href);
+// }
+
+//console.log(anchor.href);
+
+// const form = document.querySelector('form')!;
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+console.log(form.children);
+
+
+//inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+  
+  // form.children.forEach(child => {  
+  // })
+  
+  console.log(
+    type.value,
+    tofrom.value,
+    details.value,
+    amount.valueAsNumber
+  )
+  
+})
