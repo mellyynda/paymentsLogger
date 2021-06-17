8e884a34719d5241df38a2b7343633146c80b72e
Melinda <melinda.sava.td@gmail.com>
2021-06-17 15:17:13 +0200

    netNinja Typescript tutorial #17-Rendering an HTML Template

--- public/app.js
+++ public/app.js
@@ -1,44 +1,17 @@
-//interfaces
-// interface isPerson {
-//   name: string;
-//   age: number;
-//   speak(a: string): void;
-//   spend(b: number): number;
-// }
-// const me: isPerson = {
-//   name:'Mely', 
-//   age: 8,
-//   speak(text: string): void {
-//     console.log(text);
-//   },
-//   spend(amount: number): number {
-//     console.log('i have spent:', amount);
-//     return amount    
-//   },
-// };
-// const greetPerson = (person: isPerson) => {
-//   console.log('Hello', person.name)
-// }
-// greetPerson(me);
-// console.log(me);
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
-// let docOne: HasFormater;
-// let docTwo: HasFormater;
-// docOne = new Invoice('mely', 'web work', 250);
-// docTwo = new Payment('mely', 'cool app', 5000);
-let docs = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
 //dom interaction
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
+// console.log(form.children);
 //inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -48,16 +21,6 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    docs.push(doc);
-    console.log(docs);
+    list.render(doc, type.value, 'start');
+    console.log(doc);
 });
-const invOne = new Invoice('mario', 'work on mario website', 250);
-const invTwo = new Invoice('louigi', 'work on louigi website', 300);
-// console.log(invOne, invTwo);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invTwo.amount = 200;
-// invOne.client = 'yoshi';
-invoices.forEach(inv => console.log(inv.client, inv.amount, inv.format()));
-//console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,24 @@
-//interfaces
-// interface isPerson {
-//   name: string;
-//   age: number;
-//   speak(a: string): void;
-//   spend(b: number): number;
-// }
-
-// const me: isPerson = {
-//   name:'Mely', 
-//   age: 8,
-//   speak(text: string): void {
-//     console.log(text);
-//   },
-//   spend(amount: number): number {
-//     console.log('i have spent:', amount);
-//     return amount    
-//   },
-// };
-
-// const greetPerson = (person: isPerson) => {
-//   console.log('Hello', person.name)
-// }
-
-// greetPerson(me);
-
-// console.log(me);
-
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormater } from './interfaces/HasFormater.js';
-
-// let docOne: HasFormater;
-// let docTwo: HasFormater;
-
-// docOne = new Invoice('mely', 'web work', 250);
-// docTwo = new Payment('mely', 'cool app', 5000);
-
-let docs: HasFormater[] = [];
-
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
 
 //dom interaction
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
-console.log(form.children);
-
+// console.log(form.children);
 
 //inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+
+const list = new ListTemplate(ul);
+
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
@@ -71,29 +35,10 @@ form.addEventListener('submit', (e: Event) => {
       amount.valueAsNumber)
   }
 
-  docs.push(doc);
+  list.render(doc, type.value , 'start');
   
   console.log(
-    docs
+    doc
   )
   
 })
-
-
-
-const invOne = new Invoice('mario', 'work on mario website', 250);
-const invTwo = new Invoice('louigi', 'work on louigi website', 300);
-
-// console.log(invOne, invTwo);
-
-let invoices: Invoice[] = [];
-
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invTwo.amount = 200;
-// invOne.client = 'yoshi';
-
-invoices.forEach(inv => console.log(inv.client, inv.amount, inv.format()));
-
-//console.log(invoices);
